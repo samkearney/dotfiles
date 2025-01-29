11198425667791ab7785f6ded4c9df57d1bfc3a6
Sam Kearney <samuel.kearney@taittowers.com>
2025-01-29 15:53:04 -0600

    Octo.nvim and mini.files

---
+++ .config/nvim/lua/plugins/octo-nvim.lua
@@ -0,0 +1,11 @@
+return {
+  "pwntester/octo.nvim",
+  opts = {
+    picker = "fzf-lua",
+  },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "ibhagwan/fzf-lua",
+    "nvim-tree/nvim-web-devicons",
+  },
+}
