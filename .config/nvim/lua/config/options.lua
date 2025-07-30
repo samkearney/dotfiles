01917ba049a9f45202a6eb7b23c94b5e3410ff93
Sam Kearney <sam.kearney@meraki.net>
2025-07-30 12:01:51 -0700

    Enable clipboard access on SSH

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,3 +1,14 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+vim.opt.diffopt = 'internal,filler,closeoff,linematch:40,followwrap'
+vim.lsp.set_log_level("trace")
+
+local ssh_conn = os.getenv("SSH_CONNECTION")
+local tmux = os.getenv("TMUX")
+local devc = os.getenv("IS_DEVCONTAINER")
+
+if (ssh_conn and ssh_conn ~= "") or (tmux and tmux ~= "") or (devc and devc ~= "") then
+  vim.opt.mouse = 'nvi'
+  vim.g.clipboard = 'osc52'
+end
