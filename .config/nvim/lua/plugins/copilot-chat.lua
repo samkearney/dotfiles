0cc0f9fc0d9ffd1492bb88a65911b8e22f916fbe
Sam Kearney <skearney@meraki.com>
2025-09-23 19:11:42 +0000

    Add copilot chat plugin

---
+++ .config/nvim/lua/plugins/copilot-chat.lua
@@ -0,0 +1,16 @@
+return {
+  "CopilotC-Nvim/CopilotChat.nvim",
+  dependencies = {
+    { "nvim-lua/plenary.nvim", branch = "master" },
+  },
+  build = "make tiktoken",
+  opts = {
+    model = 'claude-sonnet-4',
+    temperature = 0.1,
+    window = {
+      layout = 'vertical',
+      width = 0.5
+    },
+    auto_insert_mode = true
+  },
+}
