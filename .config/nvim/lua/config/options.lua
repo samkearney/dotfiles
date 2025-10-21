ad9d999834f309836e782379d9cf081792439f5a
Sam Kearney <samuelmkearney@gmail.com>
2025-10-20 21:15:19 -0500

    Turn off lsp logging

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,14 +1,13 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.opt.diffopt = 'internal,filler,closeoff,linematch:40,followwrap'
-vim.lsp.set_log_level("trace")
+vim.opt.diffopt = "internal,filler,closeoff,linematch:40,followwrap"
+vim.lsp.set_log_level("off")
 
 local ssh_conn = os.getenv("SSH_CONNECTION")
-local tmux = os.getenv("TMUX")
 local devc = os.getenv("IS_DEVCONTAINER")
 
-if (ssh_conn and ssh_conn ~= "") or (tmux and tmux ~= "") or (devc and devc ~= "") then
-  vim.opt.mouse = 'nvi'
-  vim.g.clipboard = 'osc52'
+if (ssh_conn and ssh_conn ~= "") or (devc and devc ~= "") then
+  vim.opt.mouse = "nvi"
+  vim.g.clipboard = "osc52"
 end
